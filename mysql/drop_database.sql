0582b4de190825041fdf2a57675776b273ed068e
JasonRenBang <54545506+JasonRenBang@users.noreply.github.com>
2020-09-24 11:23:02 +0800

    Update drop_database.sql

--- mysql/drop_database.sql
+++ mysql/drop_database.sql
@@ -2,6 +2,7 @@
  * drop_database - drops the MealPlanner database
  *  
  * Author: Garrick Smith
+ *         Zhichen Ren
  * Revision Date: 9/29/19
  ******************************************************************************/
  DROP DATABASE MealPlanner;
